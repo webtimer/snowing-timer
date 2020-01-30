a0e999fd35c8789bc7576ed3225eea454ca81f2b
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-01-30 23:13:43 +0900

    Update to use Webpack

--- src/index.js
+++ src/index.js
@@ -1,81 +1,87 @@
-let snowflakes = []; // array to hold snowflake objects
-let piledSnowflakes = [];
+import P5 from "p5";
 
-let piledSpeed = 0.05;
-let piledHeight = -300 * piledSpeed;
+let sketch = function(p) {
+  let snowflakes = []; // array to hold snowflake objects
+  let piledSnowflakes = [];
 
-function setup() {
-  createCanvas(400, 600);
-  fill(240);
-  noStroke();
-}
+  let piledSpeed = 0.05;
+  let piledHeight = -300 * piledSpeed;
 
-function draw() {
-  background('brown');
-  let t = frameCount / 60; // update time
+  p.setup = function() {
+    p.createCanvas(400, 600);
+    p.fill(240);
+    p.noStroke();
+  };
 
-  // create a random number of snowflakes each frame
-  for (var i = 0; i < random(5); i++) {
-    snowflakes.push(new snowflake()); // append snowflake object
-  }
+  p.draw = function() {
+    p.background("brown");
+    let t = p.frameCount / 60; // update time
 
-  // loop through snowflakes with a for..of loop
-  for (let flake of snowflakes) {
-    flake.update(t); // update snowflake position
-    flake.display(); // draw snowflake
-  }
+    // create a random number of snowflakes each frame
+    for (var i = 0; i < p.random(5); i++) {
+      snowflakes.push(new snowflake()); // append snowflake object
+    }
 
-  // draw piled snow using rectangle
-  piledHeight += piledSpeed;    
-  fill('white');
-  rect(0, height - piledHeight, width, piledHeight);
-  // draw piledSnowflakes
-  for (let piledFlake of piledSnowflakes) {
-    piledFlake.deleteBuriedSnowflakes();
-    piledFlake.display();
-  }
-}
+    // loop through snowflakes with a for..of loop
+    for (let flake of snowflakes) {
+      flake.update(t); // update snowflake position
+      flake.display(); // draw snowflake
+    }
 
-// snowflake class
-function snowflake() {
-  // initialize coordinates
-  this.posX = 0;
-  this.posY = random(-50, 0);
-  this.initialangle = random(0, 2 * PI);
-  this.size = random(2, 5);
-
-  // radius of snowflake spiral
-  // chosen so the snowflakes are uniformly spread out in area
-  this.radius = sqrt(random(pow(width / 2, 2)));
-
-  this.update = function(time) {
-    // x position follows a circle
-    let w = 0.6; // angular speed
-    let angle = w * time + this.initialangle;
-    this.posX = width / 2 + this.radius * sin(angle);
-
-    // different size snowflakes fall at slightly different y speeds
-    this.posY += pow(this.size, 0.5);
-
-    // delete snowflake if past end of piled snow line
-    if (this.posY > height - piledHeight) {
-      // add piledSnowflake instead of deleting snowflake
-      piledSnowflakes.push(this);
-
-      let index = snowflakes.indexOf(this);
-      snowflakes.splice(index, 1);
+    // draw piled snow using rectangle
+    piledHeight += piledSpeed;
+    p.fill("white");
+    p.rect(0, p.height - piledHeight, p.width, piledHeight);
+    // draw piledSnowflakes
+    for (let piledFlake of piledSnowflakes) {
+      piledFlake.deleteBuriedSnowflakes();
+      piledFlake.display();
     }
   };
 
-  this.display = function() {
-    ellipse(this.posX, this.posY, this.size);
-  };
+  // snowflake class
+  let snowflake = function() {
+    // initialize coordinates
+    this.posX = 0;
+    this.posY = p.random(-50, 0);
+    this.initialangle = p.random(0, 2 * p.PI);
+    this.size = p.random(2, 5);
 
-  // delete piledSnowflakes under piled snow line
-  this.deleteBuriedSnowflakes = function() {
-    if (this.posY > height - piledHeight + 5) {
-      let index = piledSnowflakes.indexOf(this);
-      piledSnowflakes.splice(index, 1);
-    }
+    // radius of snowflake spiral
+    // chosen so the snowflakes are uniformly spread out in area
+    this.radius = p.sqrt(p.random(p.pow(p.width / 2, 2)));
+
+    this.update = function(time) {
+      // x position follows a circle
+      let w = 0.6; // angular speed
+      let angle = w * time + this.initialangle;
+      this.posX = p.width / 2 + this.radius * p.sin(angle);
+
+      // different size snowflakes fall at slightly different y speeds
+      this.posY += p.pow(this.size, 0.5);
+
+      // delete snowflake if past end of piled snow line
+      if (this.posY > p.height - piledHeight) {
+        // add piledSnowflake instead of deleting snowflake
+        piledSnowflakes.push(this);
+
+        let index = snowflakes.indexOf(this);
+        snowflakes.splice(index, 1);
+      }
+    };
+
+    this.display = function() {
+      p.ellipse(this.posX, this.posY, this.size);
+    };
+
+    // delete piledSnowflakes under piled snow line
+    this.deleteBuriedSnowflakes = function() {
+      if (this.posY > p.height - piledHeight + 5) {
+        let index = piledSnowflakes.indexOf(this);
+        piledSnowflakes.splice(index, 1);
+      }
+    };
   };
-}
+};
+
+new P5(sketch);
