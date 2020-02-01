beb9c9af228aaa0634bafb9106f84ee29a6c09b5
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-02-01 23:21:07 +0900

    Refactor snowflake.js

--- src/snowflake.js
+++ src/snowflake.js
@@ -3,7 +3,7 @@
  */
 export default class Snowflake {
   constructor(p) {
-    // 初期化
+    this.p = p
     this.posX = 0
     this.posY = p.random(-50, 0)
     this.initialangle = p.random(0, 2 * p.PI)
@@ -11,38 +11,35 @@ export default class Snowflake {
     // radius of snowflake spiral
     // chosen so the snowflakes are uniformly spread out in area
     this.radius = p.sqrt(p.random(p.pow(p.width / 2, 2)))
+  }
 
-    this.updatePosition = function(
-      time,
-      snowflakes,
-      piledSnowflakes,
-      piledHeight
-    ) {
-      // x position follows a circle
-      let w = 0.6 // angular speed
-      let angle = w * time + this.initialangle
-      this.posX = p.width / 2 + this.radius * p.sin(angle)
-      // different size snowflakes fall at slightly different y speeds
-      this.posY += p.pow(this.size, 0.5)
-      // delete snowflake if past end of piled snow line
-      if (this.posY > p.height - piledHeight) {
-        // add piledSnowflake instead of deleting snowflake
-        piledSnowflakes.push(this)
-        let index = snowflakes.indexOf(this)
-        snowflakes.splice(index, 1)
-      }
+  updatePosition(time, snowflakes, piledSnowflakes, piledHeight) {
+    // x position follows a circle
+    let w = 0.6 // angular speed
+    let angle = w * time + this.initialangle
+    this.posX = this.p.width / 2 + this.radius * this.p.sin(angle)
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += this.p.pow(this.size, 0.5)
+    // delete snowflake if past end of piled snow line
+    if (this.posY > this.p.height - piledHeight) {
+      // add piledSnowflake instead of deleting snowflake
+      piledSnowflakes.push(this)
+      let index = snowflakes.indexOf(this)
+      snowflakes.splice(index, 1)
     }
+  }
 
-    this.display = function() {
-      p.ellipse(this.posX, this.posY, this.size)
-    }
+  display() {
+    this.p.ellipse(this.posX, this.posY, this.size)
+  }
 
-    // delete piledSnowflakes under piled snow line
-    this.deleteBuriedSnowflakes = function(piledSnowflakes, piledHeight) {
-      if (this.posY > p.height - piledHeight + 5) {
-        let index = piledSnowflakes.indexOf(this)
-        piledSnowflakes.splice(index, 1)
-      }
+  /**
+   * 積もった雪の下に埋め込まれた雪片を削除します。
+   */
+  deleteBuriedSnowflakes(piledSnowflakes, piledHeight) {
+    if (this.posY > this.p.height - piledHeight + 5) {
+      let index = piledSnowflakes.indexOf(this)
+      piledSnowflakes.splice(index, 1)
     }
   }
 }
